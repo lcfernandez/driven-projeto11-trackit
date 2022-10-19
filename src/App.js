9e28f4475c6a10f7725d7221782694b73080e84a
lcfernandez <lcfernandez@cos.ufrj.br>
2022-10-18 21:24:09 -0300

    GlobalStyle e Header da Home

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import GlobalStyle from "./assets/styles/GlobalStyle";
+
 import HomePage from "./pages/HomePage/HomePage";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -5,6 +7,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
+        <GlobalStyle />
+        
         <Routes>
             <Route path="/" element={<HomePage />} />
         </Routes>
--- src/components/HeaderHome/HeaderHome.js
+++ src/components/HeaderHome/HeaderHome.js
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+
 import logo from "../../assets/img/logo.png";
 
 export default function HeaderHome() {
-    return <img alt="TrackIt logo" src={logo}/>;
+    return (
+        <HeaderHomeContainer>
+            <div>
+            <img alt="TrackIt logo" src={logo}/>
+            </div>
+        </HeaderHomeContainer>
+    );
 }
+
+const HeaderHomeContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 68px 0 32px 0;
+`;
